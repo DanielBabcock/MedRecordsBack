8fc95acde22196200b302e449c1bf788d53797e2
DanielBabcock <babcock.daniel.b@gmail.com>
2017-12-14 18:06:44 -0600

    update, need to finish the recall_all_controller and coresponding route.rb next

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins '*'
+    # asterisk is opening it wide up, fix to more specific later for security.
+    # or use     
+    # origins 'localhost:4200'  or make it specific like ( http://127.0.0.1:8080/#!/ )  used this tutorial: https://til.hashrocket.com/posts/4d7f12b213-rails-5-api-and-cors
 
     resource '*',
       headers: :any,
